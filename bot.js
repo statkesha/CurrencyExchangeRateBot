0dd9c909a845e145b5093d94806dc3cf0e0d2696
Hanna <anyta167@gmail.com>
2020-11-17 22:48:00 +0300

    some small changes

--- bot.js
+++ bot.js
@@ -3,16 +3,14 @@ const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const cc = require('currency-codes');
 const Markup = require('telegraf/markup');
-//const { oneTime } = require('telegraf/markup');
 
-//const api = axios.get('https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates');
 
- 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name}!`,
+bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name}!
+Узнай курс валют Альфа-банка.
+Введи на английском валюту (usd, eur, rub) и увидишь курс за сегодня.`,
 Markup.keyboard([
-    ['Alfa-bank', 'НБ'],
-    ['Приорбанк','Беларусбанк'],
+    ['USD','EUR','RUB'],
 ])
 .resize()
 .extra()
@@ -25,19 +23,8 @@ Markup.keyboard([
 //  });
 
 
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => ctx.reply('Send me currency code'))
 
-// bot.hears(/^[A-Z]+$/i,(ctx) => {
-//    var request = require("request");
-// var options = { method: 'GET',
-//   url: 'https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates'};
-
-// request(options, function (error, response, body) {
-//   // if (error) throw new Error(error);
-
-//  ctx.reply(body);
-// })
-// });
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 
 bot.hears(/^[A-Z]+$/i, async(ctx) => {
@@ -57,17 +44,19 @@ const foundCurrency = currObj.data.rates.find((cur)=>{
 const foundSecondCurrency = currObj.data.rates.find((cur)=>{
     return cur.buyCode.toString() === currency.number;
    });
-if(!foundCurrency || !foundCurrency.sellRate || !foundCurrency.buyRate){
+   // !foundCurrency.sellRate || !foundCurrency.buyRate
+if(!foundCurrency && !foundSecondCurrency){
     return ctx.reply('Валюта не найдена в Alfa-bank api');
 }
 return ctx.replyWithMarkdown(`
-Валюта: ${currency.code}
+Курс:${foundCurrency.sellIso} - ${foundCurrency.buyIso}
 Курс покупки: *${foundCurrency.buyRate}*
 Курс продажи: *${foundCurrency.sellRate}*
+
 `);
 }catch(error){
     return ctx.reply(error)
  }
 });
-
+// ${foundCurrency.quantity} ${foundCurrency.name.toString()}
 bot.launch();
