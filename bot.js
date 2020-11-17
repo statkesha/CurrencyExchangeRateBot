05e31dd088d78a59edcd10b78bb63dda889ce55c
Hanna <anyta167@gmail.com>
2020-11-17 16:51:52 +0300

    bot use alfabankapi

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const cc = require('currency-codes');
 const Markup = require('telegraf/markup');
+//const { oneTime } = require('telegraf/markup');
+
+//const api = axios.get('https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates');
 
  
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -15,53 +18,56 @@ Markup.keyboard([
 .extra()
 )
 );
-// bot.on ('text', (ctx)=>{
-//     let data = {};
-//     data = api.getsmth;
-//     ctx.reply(data);
-// https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates
-// })
+//  bot.on ('text', (ctx)=>{
+//     let data = {}
+//     data = api.get('rates'); 
+//      ctx.reply(data);
+//  });
+
 
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 
-bot.hears(/^[A-Z]+$/i, async(ctx) => {
-   var request = require("request");
-var options = { method: 'GET',
-  url: 'https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates' };
+// bot.hears(/^[A-Z]+$/i,(ctx) => {
+//    var request = require("request");
+// var options = { method: 'GET',
+//   url: 'https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates'};
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+// request(options, function (error, response, body) {
+//   // if (error) throw new Error(error);
 
-  console.log(response);
-})
-});
+//  ctx.reply(body);
+// })
+// });
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-// bot.hears(/^[A-Z]+$/i, async(ctx) => {
-//     const clientCurCode = ctx.message.text;
-//     const currency = cc.code(clientCurCode);
-//     console.log(currency);
-// //checking if currency exist
-//     if (!currency){
-//         return ctx.reply('Валюта не найдена');
-//     }
-//     try{
-//         const currencyObj = await axios.get('https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates')
-   
-//    const foundCurrency = currencyObj.data.find((cur)=>{
-//        return cur.sellCode === currency.number;
-//    });
-//    if(!foundCurrency || !foundCurrency.rates.sellRate || !foundCurrency.rates.buyRate){
-//        return ctx.reply('Currency didnt found in Alfa-bank api');
-//    }
-//         return ctx.replyWithMarkdown(
-//            `
-//             Валюта: ${currency.rates}
-//             Rate sell: *${foundCurrency.rates.sellRate}*
-//             Rate buy: *${foundCurrency.rates.buyRate}*
-//            ` );
-//     }catch(error){
-//         return ctx.reply(error);
-//     }
-// }
-// );
-bot.launch()
+
+bot.hears(/^[A-Z]+$/i, async(ctx) => {
+    const clientCurCode = ctx.message.text;
+    const currency = cc.code(clientCurCode);
+    console.log(currency);
+//checking if currency exist
+    if (!currency){
+        return ctx.reply('Валюта не найдена');
+    }
+
+    try{
+const currObj = await axios.get('https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates')
+const foundCurrency = currObj.data.rates.find((cur)=>{
+ return cur.sellCode.toString() === currency.number;
+});
+const foundSecondCurrency = currObj.data.rates.find((cur)=>{
+    return cur.buyCode.toString() === currency.number;
+   });
+if(!foundCurrency || !foundCurrency.sellRate || !foundCurrency.buyRate){
+    return ctx.reply('Валюта не найдена в Alfa-bank api');
+}
+return ctx.replyWithMarkdown(`
+Валюта: ${currency.code}
+Курс покупки: *${foundCurrency.buyRate}*
+Курс продажи: *${foundCurrency.sellRate}*
+`);
+}catch(error){
+    return ctx.reply(error)
+ }
+});
+
+bot.launch();
